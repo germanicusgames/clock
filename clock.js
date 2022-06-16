3295b7a8e3b544d9f0f5e696464514fa5d1a816f
germanicusgames <83325630+germanicusgames@users.noreply.github.com>
2022-06-16 11:24:16 -0400

    Update clock.js

--- clock.js
+++ clock.js
@@ -6,7 +6,26 @@ radius = radius * 0.90
 drawClock();
 
 function drawClock() {
+   drawFace(ctx, radius);
+}
+
+function drawFace(ctx, radius) {
+    var grad;
+    ctx.beginpath();
     ctx.arc(0,0,radius,0,2*Math.PI);
     ctx.fillstyle ="white";
     ctx.fill();
+    grad = ctx.createradialgradient(0,0,radius*0.95, 0,0,radius*1.05);
+    grad.addColorStop(0, '#333');
+    grad.addColorStop(0.5, 'white');
+    grad.addColorStop(1, '#333');
+    ctx.strokestyle = grad;
+    ctx.linewidth = radius*0.1;
+    ctx.stroke();
+    ctx.beginpath();
+    ctx.arc(0, 0, radius*0.1, 0, 2*Math.PI);
+    ctx.fillstyle = '#333';
+    ctx.fill();
 }
+
+
